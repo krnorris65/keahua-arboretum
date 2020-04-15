afe6f743dc35b430ebee6b7da87318c05be05c65
Kristen Norris <krnorris65@gmail.com>
2020-04-15 14:44:49 -0500

    error handling for if user enters incorrect or invalid character

--- actions/choose_annex.py
+++ actions/choose_annex.py
@@ -2,31 +2,54 @@ import os
 from environments import River, Coastline
 
 
-def choose_annex(arboretum, animal, options, display):
+def choose_annex(arboretum, animal, options, display="initial"):
     os.system('cls' if os.name == 'nt' else 'clear')
 
-    if display == "error":
-        print("You can't select this option")
 
+    if display == "initial":
+        print("**** Please select a biome ****")
+
+    if display == "error":
+        print("**** That biome is not large enough, please choose another one ****")
+
+    if display == "invalid":
+        print("**** Invalid Selection, please choose another one ****")
+    
+    print("(Press enter to return to Main Screen)")
+    print("")
+
+    
     for index, option in enumerate(options):
         if(type(option) == River):
             print(f'{index + 1}. River ({len(option.animals)} animals)')
         elif(type(option) == Coastline):
             print(f'{index + 1}. Coastline ({len(option.animals)} animals)')
-    
-    print("Release the animal into which biome?")
-    choice = input("> ")
-    chosen_option = options[int(choice) - 1]
 
-    # find the index of the chosen option in the correct list and add the animal to that list
-    if(type(chosen_option) ==River):
-        river_index = arboretum.rivers.index(chosen_option)
-        river = arboretum.rivers[river_index]
-        if not river.animals_at_capacity:
-            river.add_animal(animal)
-        else:
-            print("this is not available")
-            choose_annex(arboretum, animal, options, "error")
-    elif(type(chosen_option) == Coastline):
-        coastline_index = arboretum.coastlines.index(chosen_option)
-        arboretum.coastlines[coastline_index].add_animal(animal)
+    print("")
+    print("Release the animal into which biome?")
+
+    choice = input("> ")
+    try:
+        chosen_option = options[int(choice) - 1]
+
+        # find the index of the chosen option in the correct list and add the animal to that list
+        if(type(chosen_option) ==River):
+            river_index = arboretum.rivers.index(chosen_option)
+            river = arboretum.rivers[river_index]
+            if not river.animals_at_capacity:
+                river.add_animal(animal)
+            else:
+                # raise exception when animal can't be added to biome
+                raise Exception  
+        elif(type(chosen_option) == Coastline):
+            coastline_index = arboretum.coastlines.index(chosen_option)
+            arboretum.coastlines[coastline_index].add_animal(animal)
+    except Exception:
+        # when an exception is raised from a conditional, run choose_annex function with display="error"
+        choose_annex(arboretum, animal, options, "error")
+    except IndexError:
+        # User entered invalid option, run choose_annex function with display="invalid"
+        choose_annex(arboretum, animal, options, "invalid")
+    except ValueError:
+        # User entered value other than a number
+        pass
--- actions/release_animal.py
+++ actions/release_animal.py
@@ -17,40 +17,9 @@ def release_animal(arboretum):
 
         # river dolphin can go in either a river or coastline
         annex_options = arboretum.rivers + arboretum.coastlines
-        choose_annex(arboretum, animal, annex_options, "initial")
+        choose_annex(arboretum, animal, annex_options)
     if choice == "2":
         pass
 
-    # iterate through all options for that animal
-    # for index, option in enumerate(annex_options):
-    #     if(type(option) == River):
-    #         print(f'{index + 1}. River ({len(option.animals)} animals)')
-    #     elif(type(option) == Coastline):
-    #         print(f'{index + 1}. Coastline ({len(option.animals)} animals)')
-    
-    # print("Release the animal into which biome?")
-    # choice = input("> ")
-    # chosen_option = annex_options[int(choice) - 1]
-
-    # find the index of the chosen option in the correct list and add the animal to that list
-    # if(type(chosen_option) ==River):
-    #     river_index = arboretum.rivers.index(chosen_option)
-    #     river = arboretum.rivers[river_index]
-    #     if not river.animals_at_capacity:
-    #         river.add_animal(animal)
-    #     else:
-    #         return
-    #         # chosen_option = choose_annex(annex_options, "error")
-    # elif(type(chosen_option) == Coastline):
-    #     coastline_index = arboretum.coastlines.index(chosen_option)
-    #     arboretum.coastlines[coastline_index].add_animal(animal)
-
-
-
-
-
-
-
-    # arboretum.rivers[int(choice) - 1].animals.append(animal)
 
 

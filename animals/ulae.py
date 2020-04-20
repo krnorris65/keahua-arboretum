7644fa85e6aa5e936f71b2fe06336b13d0a9b2f6
Kristen Norris <krnorris65@gmail.com>
2020-04-20 13:51:33 -0500

    user can add an ulae to a coastline

---
+++ animals/ulae.py
@@ -0,0 +1,11 @@
+from animals import Animal
+from interfaces import Identifiable, ISwimming, ISaltwater
+
+class Ulae(Animal, Identifiable, ISwimming, ISaltwater):
+
+    def __init__(self):
+        Animal.__init__(self, "'Ulae'")
+        Identifiable.__init__(self)
+        ISwimming.__init__(self)
+        ISaltwater.__init__(self)
+        self.__prey = { "Fish"}

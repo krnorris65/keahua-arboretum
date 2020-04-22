3523d94d94fece612021e3337a388a6c8c3f2f09
Kristen Norris <krnorris65@gmail.com>
2020-04-22 09:37:40 -0500

    created a package for actions to condense imports on index.oy

--- index.py
+++ index.py
@@ -1,8 +1,7 @@
 import os
 from arboretum import Arboretum
-from actions.annex import annex_habitat
-from actions.release_animal import release_animal
-from actions.report import build_facility_report
+from actions import annex_habitat, release_animal, feed_animal, report
+
 
 keahua = Arboretum("Keahua Arboretum", "123 Paukauila Lane")
 
@@ -32,7 +31,7 @@ def main_menu():
         release_animal(keahua)
 
     if choice == "3":
-        pass
+        feed_animal(keahua)
 
     if choice == "4":
         pass

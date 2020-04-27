8954afa636b251d785edf4e8c4b8ae60e8915958
Kristen Norris <krnorris65@gmail.com>
2020-04-27 14:26:40 -0500

    updated helper function to be for either animals or plants

--- helper/biome_options.py
+++ helper/biome_options.py
@@ -1,54 +1,54 @@
-def get_biome_options(animal, arboretum):
+def get_biome_options(inhabitant, arboretum):
     '''
-        Determines what biomes an animal can be released to based on if that animal meets certain conditions
+        Determines what biomes an animal or plant can be released to based on if that animal meets certain conditions
 
         Arguments: 
-            animal that is being released
-            arboretum that animal will be released to
+            animal or plant that is being released
+            arboretum that animal or plant will be released to
             
         Returns:
-            list of biomes in the arboretum that the animal can be released to
+            list of biomes in the arboretum that the animal or plant can be released to
     '''
     biomes = []
 
     try: 
         # meets these conditions add rivers
-        if animal.aquatic and (animal.cell_type == "hypertonic" or animal.cell_type == "isotonic"):
+        if inhabitant.aquatic and (inhabitant.cell_type == "hypertonic" or inhabitant.cell_type == "isotonic"):
             biomes.extend(arboretum.rivers)
     except:
         pass
 
     try:
         # meets these conditions add coastlines
-        if animal.aquatic and (animal.cell_type == "hypotonic" or animal.cell_type == "isotonic"):
+        if inhabitant.aquatic and (inhabitant.cell_type == "hypotonic" or inhabitant.cell_type == "isotonic"):
             biomes.extend(arboretum.coastlines)
     except:
         pass
     
     try:
         # meets these conditions add forests
-        if animal.terrestrial and animal.can_be_in_shade:
+        if inhabitant.terrestrial and inhabitant.can_be_in_shade:
             biomes.extend(arboretum.forests)
     except:
         pass
     
     try:
         # meets these conditions add grasslands
-        if animal.terrestrial and animal.can_handle_less_rain:
+        if inhabitant.terrestrial and inhabitant.can_handle_less_rain:
             biomes.extend(arboretum.grasslands)
     except:
         pass
     
     try:
         # meets these conditions add swamps
-        if animal.can_be_in_stagnant_env == True:
+        if inhabitant.can_be_in_stagnant_env == True:
             biomes.extend(arboretum.swamps)
     except:
         pass
     
     try:
         # meets these conditions add mountains
-        if animal.terrestrial and animal.can_handle_high_elevation:
+        if inhabitant.terrestrial and inhabitant.can_handle_high_elevation:
             biomes.extend(arboretum.mountains)
     except:
         pass

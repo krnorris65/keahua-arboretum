90587b53ceb0c000f7b5af0e66eec6fb6e785b61
Kristen Norris <krnorris65@gmail.com>
2020-04-27 14:30:22 -0500

    updated IContainsPlants interface to have plant capacity

--- interfaces/habitat/contains_plants.py
+++ interfaces/habitat/contains_plants.py
@@ -1,4 +1,16 @@
 class IContainsPlants():
 
     def __init__(self):
-        self.plants = []
+        self.__plants = []
+        self.plant_capacity = 0
+    
+    @property
+    def plants(self):
+        return self.__plants
+        
+    @property
+    def plants_at_capacity(self):
+        if len(self._IContainsPlants__plants) == self.plant_capacity:
+            return True
+        else:
+            return False
